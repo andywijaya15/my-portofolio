0b1dabc28d136d713653b1b7d8a11f489c200d12
andywijaya15 <andykevonata@gmail.com>
2022-08-09 21:38:54 +0700

    add socket.io

--- app/application/views/realtime/realtime.php
+++ app/application/views/realtime/realtime.php
@@ -1,6 +1,16 @@
 <?php $this->view("_layouts/header") ?>
 <h1 class="app-page-title">Realtime Apps</h1>
 
+<div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+
+
 <script src="/assets/plugins/socket.io/socket.io.min.js"></script>
 <script>
     const socket = io("https://socket-ndik.herokuapp.com");
