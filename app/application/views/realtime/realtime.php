912ec219fe1f1469d9e8b026b640760e95faa7ec
andywijaya15 <andykevonata@gmail.com>
2022-08-09 21:19:05 +0700

    add socket.io

--- app/application/views/realtime/realtime.php
+++ app/application/views/realtime/realtime.php
@@ -1,5 +1,17 @@
 <?php $this->view("_layouts/header") ?>
 <h1 class="app-page-title">Realtime Apps</h1>
 
+<script src="/assets/plugins/socket.io/socket.io.min.js"></script>
+<script>
+    const socket = io("https://socket-ndik.herokuapp.com");
+
+    socket.emit("connected", {
+        username: '<?= $this->session->userdata("nama_user"); ?>'
+    });
+
+    socket.on("nyambung", (res) => {
+        console.log(res);
+    });
+</script>
 
 <?php $this->view("_layouts/footer") ?>
