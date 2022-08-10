aeb5a185e24f92cfa12d260c3eeee8d77af201cd
andywijaya15 <andykevonata@gmail.com>
2022-08-10 09:30:32 +0700

    update realtime

--- app/application/views/realtime/realtime.php
+++ app/application/views/realtime/realtime.php
@@ -1,26 +1,48 @@
 <?php $this->view("_layouts/header") ?>
 <h1 class="app-page-title">Realtime Apps</h1>
 
-<div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+<div class="card">
+    <div class="card-header">
+        Realtime data dengan node js
+    </div>
+    <div class="card-body" id="offline">
+        <!-- <h5 class="card-title">Realtime data dengan node js</h5> -->
+        <p class="card-text fw-bold">Fitur realtime tidak bisa digunakan karna anda tidak terhubung ke node js atau akses ke node js terblok oleh koneksi anda,silahkan gunakan koneksi internet yang lain..</p>
+    </div>
+    <div class="card-body" id="online" hidden="hidden">
+        <!-- <h5 class="card-title">Realtime data dengan node js</h5> -->
+        <p class="card-text">Silahkan buka <a href="https://ndik.helloworld.my.id/Realtime" class="fw-bold">https://ndik.helloworld.my.id/Realtime</a> di hp dan login dengan nama dan email yang berbeda lalu klik tombol dibawah ini dan lihat apa yang terjadi : </p>
+        <button class="btn btn-danger" id="btnsapa">Sapa Semua Orang yang online</button>
     </div>
 </div>
 
 
 <script src="/assets/plugins/socket.io/socket.io.min.js"></script>
 <script>
-    const socket = io("https://socket-ndik.herokuapp.com");
+    const cardOnline = document.querySelector("#online");
+    const cardOffline = document.querySelector("#offline");
+    const btnSapa = document.querySelector("#btnsapa");
 
-    socket.emit("connected", {
-        username: '<?= $this->session->userdata("nama_user"); ?>'
+    const socket = io("http://localhost:3000");
+
+    socket.on("connect", () => {
+        cardOnline.removeAttribute("hidden");
+        cardOffline.setAttribute("hidden", "hidden");
     });
 
-    socket.on("nyambung", (res) => {
-        console.log(res);
+    socket.on("disconnect", () => {
+        cardOffline.removeAttribute("hidden");
+        cardOnline.setAttribute("hidden", "hidden");
+    });
+
+    btnSapa.addEventListener("click", () => {
+        socket.emit("sapa", {
+            username: '<?= $this->session->userdata("nama_user"); ?>'
+        });
+    });
+
+    socket.on("dapetsapa", (res) => {
+        noty("success", `Halo user <?= $this->session->userdata("nama_user"); ?>,${res} menyapamu..`)
     });
 </script>
 
