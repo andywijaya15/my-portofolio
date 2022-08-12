b3e25669cc4fb6c785a90b00f7e686a4f8f45241
andywijaya15 <andykevonata@gmail.com>
2022-08-12 15:14:03 +0700

    edit dashboard

--- app/application/views/dashboard/dashboard.php
+++ app/application/views/dashboard/dashboard.php
@@ -7,7 +7,7 @@
             <div class="row gx-5 gy-3">
                 <div class="col-12 col-lg-9">
 
-                    <div>Kumpulan Fitur Project yang pernah saya buat,semua fitur disini realtime jadi silahkan buka di 2 perangkat berbeda / lebih untuk melihat data berubah secara realtim</div>
+                    <div>Kumpulan Fitur Project yang pernah saya buat,semua fitur disini realtime jadi silahkan buka di 2 perangkat berbeda / lebih untuk melihat data berubah secara realtime</div>
                 </div>
             </div>
         </div>
